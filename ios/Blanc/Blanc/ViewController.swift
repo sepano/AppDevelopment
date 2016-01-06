91cabcc7c4e59a8eb76683dfafab30784de7fcf3
Boning Gong <boninggong@Bonings-MacBook-Pro.local>
2016-01-07 00:28:13 +0100

    Side menu working

--- ios/Blanc/Blanc/ViewController.swift
+++ ios/Blanc/Blanc/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Menu: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        Menu.target = self.revealViewController()
+        Menu.action = Selector("revealToggle:")
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
     }
 
     override func didReceiveMemoryWarning() {
